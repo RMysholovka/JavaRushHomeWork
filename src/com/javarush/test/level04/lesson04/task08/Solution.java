c3bd749505e36ff606a338cb7c7f0f00c954da03
RomanMysholovka <rmysholovka@gmail.com>
2016-04-25 17:24:28 +0300

    Merge branch 'master' of https://github.com/RMysholovka/JavaRushHomeWork
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/com/javarush/test/level04/lesson04/task08/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.test.level04.lesson04.task08;
+
+/* Треугольник
+Ввести с клавиатуры три числа а, b, c – стороны предполагаемого треугольника.
+Определить возможность существования треугольника по сторонам. Результат вывести на экран в следующем виде:
+"Треугольник существует." - если треугольник с такими сторонами существует.
+"Треугольник не существует." - если треугольник с такими сторонами не существует.
+Подсказка: Треугольник существует только тогда, когда сумма любых двух его сторон больше третьей.
+Требуется сравнить каждую сторону с суммой двух других.
+Если хотя бы в одном случае сторона окажется больше суммы двух других, то треугольника с такими сторонами не существует.
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));//напишите тут ваш код
+        String number = reader.readLine();
+        int a = Integer.parseInt(number);
+
+        String number2 = reader.readLine();
+        int b = Integer.parseInt(number2);
+
+        String number3 = reader.readLine();
+        int c = Integer.parseInt(number3);
+
+        if ((a + b) <= c)
+            System.out.println("Треугольник не существует.");
+        else
+            if((b+c) <= a)
+                System.out.println("Треугольник не существует.");
+        else
+                if ((a+c) <= b)
+                    System.out.println("Треугольник не существует.");
+        else
+                    System.out.println("Треугольник существует.");
+
+    }
+}
