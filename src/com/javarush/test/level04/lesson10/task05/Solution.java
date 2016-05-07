4f8500d4b847364f1d6e30794dbd8dbb44dde9cb
RomanMysholovka <rmysholovka@gmail.com>
2016-05-07 23:49:32 +0300

    Merge branch 'master' of https://github.com/RMysholovka/JavaRushHomeWork
    # Conflicts:
    #	.idea/workspace.xml

--- src/com/javarush/test/level04/lesson10/task05/Solution.java
+++ src/com/javarush/test/level04/lesson10/task05/Solution.java
@@ -14,7 +14,18 @@ public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //напишите тут ваш код
+       int i = 1;
+        int k = 1;
+        while (i<11){
+            while(k<10){
+                System.out.print((i*k)+" ");
+                k++;
+
+            }
+            System.out.println(i*k);
+            i++;
+            k=k-9;
+        }
 
     }
 }
